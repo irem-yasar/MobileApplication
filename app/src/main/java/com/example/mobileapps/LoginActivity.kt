4dd39e75495050bc03d4853d36dd6eabfede5937
eronhetemi <ehetemi23@gmail.com>
2025-01-03 16:49:15 +0100

    Your commit message

--- app/src/main/java/com/example/mobileapps/LoginActivity.kt
+++ app/src/main/java/com/example/mobileapps/LoginActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     lateinit var credentialsManager: CredentialsManager
 
